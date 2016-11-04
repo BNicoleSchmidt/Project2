9e6854d657587d34a36c862ddbd5c8874d9e6854
BNicoleSchmidt <the.ringwyrm@gmail.com>
2016-11-03 20:53:11 -0500

    Initial commit, base template - BNS

---
+++ MeramecNetFlixProject/Data Access Layer/AccessDataSQLServer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//The System.Data.SqlClient reference is needed to access SQL Server database
+using System.Data.SqlClient;
+
+namespace MeramecNetFlixProject
+{
+    public class AccessDataSQLServer
+    {
+        public AccessDataSQLServer()
+        {
+        }
+
+        //Add public methods to expose to the outside world
+
+    }
+
+   
+}
