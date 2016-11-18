1d58998ffca1aca8041a826022aa84a7106b14fc
BNicoleSchmidt <the.ringwyrm@gmail.com>
2016-11-17 20:35:18 -0600

    add customer service and first test - nicole

---
+++ MeramecNetFlixProject.UnitTest/Services/CustomerServiceFixture.cs
@@ -0,0 +1,19 @@
+﻿using MeramecNetFlixProject.Services;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+
+namespace MeramecNetFlixProject.UnitTest.Services
+{
+    public class CustomerServiceFixture : BaseFixture
+    {
+        [Test]
+        public void ValidateLogin_Returns_False_If_Username_DNE()
+        {
+            var customerService = new CustomerService();
+
+            var actual = customerService.ValidateLogin(Random.String(), Random.String());
+
+            Assert.IsFalse(actual);
+        }
+    }
+}
