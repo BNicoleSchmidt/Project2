148beb0363471bfcba603f5481446d0c441bb373
Timothy <kainscoma@gomail.com>
2016-12-08 21:34:38 -0600

    added jpeg's for the movie covers. created the movie browser form.

---
+++ MeramecNetFlixProject/UI/BrowseInventory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MeramecNetFlixProject.UI
+{
+    public partial class BrowseInventory : Form
+    {
+        public BrowseInventory()
+        {
+            InitializeComponent();
+
+            pnlMovieScroller.AutoScroll = false;
+            pnlMovieScroller.HorizontalScroll.Enabled = false;
+            pnlMovieScroller.HorizontalScroll.Visible = false;
+            pnlMovieScroller.HorizontalScroll.Maximum = 0;
+            pnlMovieScroller.AutoScroll = true;
+        }
+
+    }
+}
