3bea1056d714af47661690b30de3fb90a1d12a3f
Timothy <kainscoma@gomail.com>
2016-12-13 18:53:17 -0600

    add web browser and made a few aesthetic changes

--- MeramecNetFlixProject/UI/MemberDataEntry.Designer.cs
+++ MeramecNetFlixProject/UI/MemberDataEntry.Designer.cs
@@ -402,7 +402,6 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Enabled = false;
             this.dataGridView1.Location = new System.Drawing.Point(13, 302);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(895, 107);
